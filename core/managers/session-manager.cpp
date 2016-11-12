1431245890663350eacff84e40636c99f941cbe0
Bartłomiej Burdukiewicz <bartlomiej.burdukiewicz@gmail.com>
2016-11-12 13:40:35 +0100

    Added warnings when using Qt 6.0.0 deprecated api.

--- core/managers/session-manager.cpp
+++ core/managers/session-manager.cpp
@@ -5,4 +5,6 @@ using namespace Manager;
 SessionManager::SessionManager(QSettings &settings, MainManager &mainManager)
 		: m_settings(settings)
 		, m_mainManager(mainManager) {
+	static_cast<void>(m_settings);
+	static_cast<void>(m_mainManager);
 }
