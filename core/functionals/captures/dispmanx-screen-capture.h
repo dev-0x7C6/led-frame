b0a140020fd072f412a078287806ae74f7f9e23f
Bartłomiej Burdukiewicz <bartlomiej.burdukiewicz@gmail.com>
2016-12-19 23:50:45 +0100

    Fixed return types for dispmanx screen capture.

--- core/functionals/captures/dispmanx-screen-capture.h
+++ core/functionals/captures/dispmanx-screen-capture.h
@@ -16,16 +16,16 @@ public:
 	virtual Enum::ScreenCaptureType type() const override;
 	virtual bool capture(ci32 id) override;
 	virtual ccolor *data() override;
-	virtual i32 width() const noexcept override { return m_w; }
-	virtual i32 height() const noexcept override { return m_h; }
+	virtual u32 width() const noexcept override { return m_w; }
+	virtual u32 height() const noexcept override { return m_h; }
 
 private:
 	DISPMANX_DISPLAY_HANDLE_T m_display = 0;
 	DISPMANX_RESOURCE_HANDLE_T m_resource = 0;
 	void *m_data = nullptr;
-	i32 m_size = 0;
-	i32 m_w = 0;
-	i32 m_h = 0;
+	u32 m_size = 0;
+	u32 m_w = 0;
+	u32 m_h = 0;
 };
 }
 }
