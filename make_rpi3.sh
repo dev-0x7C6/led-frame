5093068c48fdf69de9e13ea480f2ed06e8013802
Bartłomiej Burdukiewicz <dev.strikeu@gmail.com>
2016-12-21 18:44:33 +0000

    Fixed CXX flags for rpi3 and make_rpi3 script build release by default.

--- make_rpi3.sh
+++ make_rpi3.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-cmake . -DGUI=off -DSUPPORT_X11=off -DSUPPORT_RPI=on -DSUPPORT_RPI3=on .
+cmake . -DGUI=off -DSUPPORT_X11=off -DSUPPORT_RPI=on -DSUPPORT_RPI3=on -DCMAKE_BUILD_TYPE=Release .
