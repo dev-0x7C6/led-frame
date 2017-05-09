8aabfab9c05c4c866248550c673b1016246800d0
Bartłomiej Burdukiewicz <bartlomiej.burdukiewicz@gmail.com>
2017-05-09 23:37:28 +0200

    Small ut improvements.

--- ut/corrector-tests.cpp
+++ ut/corrector-tests.cpp
@@ -13,13 +13,13 @@ using namespace Corrector::Interface;
 using namespace Enum;
 using namespace Functional::Color;
 
-inline void correctorStandardTest(ICorrector *iface, const factor_t factor, ccolor fill, ccolor expect) {
-	Scanline stream;
-	stream.fill(fill);
+inline void correctAndCheck(ICorrector *iface, const correct_t factor, ccolor fill, ccolor expect) {
+	Scanline scanline;
+	scanline.fill(fill);
 	iface->setFactor(factor);
-	iface->correct(stream);
+	iface->correct(scanline);
 
-	for (const auto &value : stream.array())
+	for (const auto &value : scanline.array())
 		EXPECT_EQ(value, expect);
 }
 
@@ -27,48 +27,48 @@ TEST(ColorCorrectors, BrightnessCorrector) {
 	BrightnessCorrector corrector(0, 0);
 	EXPECT_EQ(corrector.factor().max(), 255);
 	EXPECT_EQ(corrector.factor().min(), 0);
-	correctorStandardTest(&corrector, 255, 0x00ffffffu, 0xffffffu);
-	correctorStandardTest(&corrector, 0, 0x00ffffffu, 0x000000u);
-	correctorStandardTest(&corrector, 127, 0x00ffffffu, 0x7f7f7fu);
-	correctorStandardTest(&corrector, 127, 0x000000ffu, 0x00007fu);
-	correctorStandardTest(&corrector, 127, 0x0000ff00u, 0x007f00u);
-	correctorStandardTest(&corrector, 127, 0x00ff0000u, 0x7f0000u);
+	correctAndCheck(&corrector, 255, 0x00ffffffu, 0x00ffffffu);
+	correctAndCheck(&corrector, 0, 0x00ffffffu, 0x00000000u);
+	correctAndCheck(&corrector, 127, 0x00ffffffu, 0x007f7f7fu);
+	correctAndCheck(&corrector, 127, 0x000000ffu, 0x0000007fu);
+	correctAndCheck(&corrector, 127, 0x0000ff00u, 0x00007f00u);
+	correctAndCheck(&corrector, 127, 0x00ff0000u, 0x007f0000u);
 }
 
 TEST(ColorCorrectors, RedCorrector) {
 	RedChannelCorrector corrector(0, 0);
 	EXPECT_EQ(corrector.factor().max(), 255);
 	EXPECT_EQ(corrector.factor().min(), 0);
-	correctorStandardTest(&corrector, 255, 0x00ffffffu, 0xffffffu);
-	correctorStandardTest(&corrector, 0, 0x00ffffffu, 0x00ffffu);
-	correctorStandardTest(&corrector, 127, 0x00ffffffu, 0x7fffffu);
-	correctorStandardTest(&corrector, 127, 0x000000ffu, 0x0000ffu);
-	correctorStandardTest(&corrector, 127, 0x0000ff00u, 0x00ff00u);
-	correctorStandardTest(&corrector, 127, 0x00ff0000u, 0x7f0000u);
+	correctAndCheck(&corrector, 255, 0x00ffffffu, 0x00ffffffu);
+	correctAndCheck(&corrector, 0, 0x00ffffffu, 0x0000ffffu);
+	correctAndCheck(&corrector, 127, 0x00ffffffu, 0x007fffffu);
+	correctAndCheck(&corrector, 127, 0x000000ffu, 0x000000ffu);
+	correctAndCheck(&corrector, 127, 0x0000ff00u, 0x0000ff00u);
+	correctAndCheck(&corrector, 127, 0x00ff0000u, 0x007f0000u);
 }
 
 TEST(ColorCorrectors, GreenCorrector) {
 	GreenChannelCorrector corrector(0, 0);
 	EXPECT_EQ(corrector.factor().max(), 255);
 	EXPECT_EQ(corrector.factor().min(), 0);
-	correctorStandardTest(&corrector, 255, 0x00ffffffu, 0xffffffu);
-	correctorStandardTest(&corrector, 0, 0x00ffffffu, 0xff00ffu);
-	correctorStandardTest(&corrector, 127, 0x00ffffffu, 0xff7fffu);
-	correctorStandardTest(&corrector, 127, 0x000000ffu, 0x0000ffu);
-	correctorStandardTest(&corrector, 127, 0x0000ff00u, 0x007f00u);
-	correctorStandardTest(&corrector, 127, 0x00ff0000u, 0xff0000u);
+	correctAndCheck(&corrector, 255, 0x00ffffffu, 0x00ffffffu);
+	correctAndCheck(&corrector, 0, 0x00ffffffu, 0x00ff00ffu);
+	correctAndCheck(&corrector, 127, 0x00ffffffu, 0x00ff7fffu);
+	correctAndCheck(&corrector, 127, 0x000000ffu, 0x000000ffu);
+	correctAndCheck(&corrector, 127, 0x0000ff00u, 0x00007f00u);
+	correctAndCheck(&corrector, 127, 0x00ff0000u, 0x00ff0000u);
 }
 
 TEST(ColorCorrectors, BlueCorrector) {
 	BlueChannelCorrector corrector(0, 0);
 	EXPECT_EQ(corrector.factor().max(), 255);
 	EXPECT_EQ(corrector.factor().min(), 0);
-	correctorStandardTest(&corrector, 255, 0x00ffffffu, 0xffffffu);
-	correctorStandardTest(&corrector, 0, 0x00ffffffu, 0xffff00u);
-	correctorStandardTest(&corrector, 127, 0x00ffffffu, 0xffff7fu);
-	correctorStandardTest(&corrector, 127, 0x000000ffu, 0x00007fu);
-	correctorStandardTest(&corrector, 127, 0x0000ff00u, 0x00ff00u);
-	correctorStandardTest(&corrector, 127, 0x00ff0000u, 0xff0000u);
+	correctAndCheck(&corrector, 255, 0x00ffffffu, 0x00ffffffu);
+	correctAndCheck(&corrector, 0, 0x00ffffffu, 0x00ffff00u);
+	correctAndCheck(&corrector, 127, 0x00ffffffu, 0x00ffff7fu);
+	correctAndCheck(&corrector, 127, 0x000000ffu, 0x0000007fu);
+	correctAndCheck(&corrector, 127, 0x0000ff00u, 0x0000ff00u);
+	correctAndCheck(&corrector, 127, 0x00ff0000u, 0x00ff0000u);
 }
 
 TEST(ColorCorrectors, AllCorrectorsAllRange) {
