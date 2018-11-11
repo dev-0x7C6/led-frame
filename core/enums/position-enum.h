0a87a927a27225c1325a16b92e8d1bf7d92a9a89
Bartłomiej Burdukiewicz <bartlomiej.burdukiewicz@gmail.com>
2018-11-11 23:09:37 +0100

    Removed unused code, make more constexpr methods.

--- core/enums/position-enum.h
+++ core/enums/position-enum.h
@@ -16,20 +16,11 @@ enum class Position : u32 {
 
 inline auto name(const Position &position) {
 	switch (position) {
-		case Position::Left:
-			return "Left";
-
-		case Position::Top:
-			return "Top";
-
-		case Position::Right:
-			return "Right";
-
-		case Position::Bottom:
-			return "Bottom";
-
-		case Position::Last:
-			return "";
+		case Position::Left: return "Left";
+		case Position::Top: return "Top";
+		case Position::Right: return "Right";
+		case Position::Bottom: return "Bottom";
+		case Position::Last: return "";
 	}
 
 	return "";
--- core/functionals/color-averaging-buffer.h
+++ core/functionals/color-averaging-buffer.h
@@ -15,63 +15,44 @@ public:
 			, m_counter(cnt) {
 	}
 
-	void clear() noexcept {
-		m_r = 0;
-		m_g = 0;
-		m_b = 0;
-		m_counter = 0;
-	}
-
-	inline void add(ccolor sample) noexcept {
-		m_r += get_r24(sample);
-		m_g += get_g24(sample);
-		m_b += get_b24(sample);
-	}
-
-	inline void setCounter(u32 counter) noexcept {
-		m_counter += counter;
-	}
-
-	inline void operator+=(ccolor sample) noexcept {
+	constexpr void operator+=(color sample) noexcept {
 		m_r += get_r24(sample);
 		m_g += get_g24(sample);
 		m_b += get_b24(sample);
 		++m_counter;
 	}
 
-	inline void operator+=(const ColorAveragingBuffer &rhs) noexcept {
+	constexpr void operator+=(const ColorAveragingBuffer &rhs) noexcept {
 		m_r += rhs.m_r;
 		m_g += rhs.m_g;
 		m_b += rhs.m_b;
 		m_counter += rhs.m_counter;
 	}
 
-	color operator()() const noexcept {
-		using counter_t = decltype(m_counter);
-		const auto avg_r = std::max(1u, m_r) / std::max(static_cast<counter_t>(1u), m_counter);
-		const auto avg_g = std::max(1u, m_g) / std::max(static_cast<counter_t>(1u), m_counter);
-		const auto avg_b = std::max(1u, m_b) / std::max(static_cast<counter_t>(1u), m_counter);
+	constexpr color operator()() const noexcept {
+		const auto avg_r = std::max(1u, m_r) / std::max(1u, m_counter);
+		const auto avg_g = std::max(1u, m_g) / std::max(1u, m_counter);
+		const auto avg_b = std::max(1u, m_b) / std::max(1u, m_counter);
 		return rgb(avg_r, avg_g, avg_b);
 	}
 
-	color bgr() const noexcept {
-		using counter_t = decltype(m_counter);
-		const auto avg_r = std::max(1u, m_r) / std::max(static_cast<counter_t>(1u), m_counter);
-		const auto avg_g = std::max(1u, m_g) / std::max(static_cast<counter_t>(1u), m_counter);
-		const auto avg_b = std::max(1u, m_b) / std::max(static_cast<counter_t>(1u), m_counter);
+	constexpr color bgr() const noexcept {
+		const auto avg_r = std::max(1u, m_r) / std::max(1u, m_counter);
+		const auto avg_g = std::max(1u, m_g) / std::max(1u, m_counter);
+		const auto avg_b = std::max(1u, m_b) / std::max(1u, m_counter);
 		return rgb(avg_b, avg_g, avg_r);
 	}
 
-	auto count() const noexcept { return m_counter; }
+	constexpr auto count() const noexcept { return m_counter; }
 
 private:
-	color m_r = 0;
-	color m_g = 0;
-	color m_b = 0;
-	u32 m_counter = 0;
+	color m_r{0};
+	color m_g{0};
+	color m_b{0};
+	u32 m_counter{0};
 };
 
 static_assert(is_class_cxx14_efficient_nothrow<ColorAveragingBuffer>::value);
 static_assert(alignof(ColorAveragingBuffer));
 static_assert(sizeof(ColorAveragingBuffer) == 16);
-}
+} // namespace Functional
