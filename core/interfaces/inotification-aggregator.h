bdab8cad4b4d38f5f510e33c28f8b4feb0ff6789
Bartłomiej Burdukiewicz <bartlomiej.burdukiewicz@gmail.com>
2019-04-14 21:07:31 +0200

    Removed missing implementation for inotification-aggregator.

--- core/interfaces/inotification-aggregator.h
+++ core/interfaces/inotification-aggregator.h
@@ -4,7 +4,7 @@ class INotification;
 
 class INotificationAggregator {
 public:
-	virtual ~INotificationAggregator();
+	virtual ~INotificationAggregator() = default;
 
 	virtual void attach(INotification &object) noexcept = 0;
 	virtual void detach(INotification &object) noexcept = 0;
